f6f18e8ce820fd16fc7eee0ebdbd8a7ea3a544d3
BhukyAkash <118367712+BhukyAkash@users.noreply.github.com>
2024-12-13 12:11:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learnt Deployment of CI/CD pipeline on AWS
         </a>
       </header>
     </div>
